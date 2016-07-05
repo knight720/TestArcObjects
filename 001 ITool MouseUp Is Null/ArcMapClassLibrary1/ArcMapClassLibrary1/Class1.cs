eaf0ff7cb5483fa63352fd94e3f8d1ce6c4a364b
Knight <ninku720@yahoo.com.tw>
2016-07-05 20:58:56 +0800

    Tool 測試

---
+++ 001 ITool MouseUp Is Null/ArcMapClassLibrary1/ArcMapClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ArcMapClassLibrary1
+{
+    [Guid("c5906d78-ae76-467f-83c1-77645fa2ade9")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("ArcMapClassLibrary1.Class1")]
+    public class Class1
+    {
+    }
+}
---
+++ 001 ITool MouseUp Is Null/ArcMapClassLibrary1/ArcMapClassLibrary1/Tool1.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.ArcMapUI;
+using System.Windows.Forms;
+
+namespace ArcMapClassLibrary1
+{
+    /// <summary>
+    /// Summary description for Tool1.
+    /// </summary>
+    [Guid("018fa017-8daa-4910-b4a1-d1d5af09c9ce")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("ArcMapClassLibrary1.Tool1")]
+    public sealed class Tool1 : BaseTool
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+
+        }
+
+        #endregion
+        #endregion
+
+        private IApplication m_application;
+        public Tool1()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "TestTool"; //localizable text 
+            base.m_caption = "TestTool";  //localizable text 
+            base.m_message = "TestTool";  //localizable text
+            base.m_toolTip = "TestTool";  //localizable text
+            base.m_name = "TestTool_TestTool";   //unique id, non-localizable (e.g. "MyCategory_ArcMapTool")
+            try
+            {
+                //
+                // TODO: change resource name if necessary
+                //
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+                base.m_cursor = new System.Windows.Forms.Cursor(GetType(), GetType().Name + ".cur");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this tool is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            m_application = hook as IApplication;
+
+            //Disable if it is not ArcMap
+            if (hook is IMxApplication)
+                base.m_enabled = true;
+            else
+                base.m_enabled = false;
+
+            // TODO:  Add other initialization code
+        }
+
+        /// <summary>
+        /// Occurs when this tool is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            // TODO: Add Tool1.OnClick implementation
+        }
+
+        public override void OnMouseDown(int Button, int Shift, int X, int Y)
+        {
+            // TODO:  Add Tool1.OnMouseDown implementation
+        }
+
+        public override void OnMouseMove(int Button, int Shift, int X, int Y)
+        {
+            // TODO:  Add Tool1.OnMouseMove implementation
+        }
+
+        public override void OnMouseUp(int Button, int Shift, int X, int Y)
+        {
+            // TODO:  Add Tool1.OnMouseUp implementation
+            MessageBox.Show(string.Format("{0}, {1}, {2}, {3}",Button,Shift,X,Y));
+        }
+        #endregion
+    }
+}
