3081addcb40d7a3725677008d495aa7ea8e7c1e2
Knight <ninku720@yahoo.com.tw>
2016-10-21 18:15:25 +0800

    測試IGxDialog

---
+++ 008 Test IGxDialog InternalCatalog/GxDialogAddin/GxDialogAddin/BtnShowDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.CatalogUI;
+using ESRI.ArcGIS.Catalog;
+
+namespace GxDialogAddin
+{
+    public class BtnShowDialog : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public BtnShowDialog()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+
+            IGxDialog gd = new GxDialog();
+            gd.InternalCatalog.Close();
+
+            IEnumGxObject ego;
+            bool result = gd.DoModalOpen(0, out ego);
+
+            int a = 0;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
